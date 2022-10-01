962922c6590db866143154a73aa85ac249c5b569
Leonardo Fernandes Jeronimo <leonardo.lfj133@gmail.com>
2022-10-01 16:59:45 -0300

    "Sobrecarga"

--- Aula13b/index.php
+++ Aula13b/index.php
@@ -3,11 +3,24 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Aula 13b - POO</title>
+        <title>Aula 13 - POO</title>
     </head>
     <body>
         <?php
-        // put your code here
+        require_once './Mamifero.php';
+        require_once './Lobo.php';
+        require_once './Cachorro.php';
+        
+        $c = new Cachorro();
+        $c->reagirFrase("Olá");
+        $c->reagirFrase("Vai apanhar!");
+        $c->reagirHora(11, 45);
+        $c->reagirHora(21, 00);
+        $c->reagirDono(true);
+        $c->reagirDono(false);
+        $c->reagirIdadePeso(2, 12.5);
+        $c->reagirIdadePeso(17, 4.5);
+        
         ?>
     </body>
 </html>
